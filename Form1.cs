f2be05be273cb59976e6cf4108e83b367543713a
Patryk Tubicz <patryk.tubicz@wp.pl>
2022-01-02 20:47:42 +0100

    Derivatives, close, round after coma

--- Form1.cs
+++ Form1.cs
@@ -13,51 +13,30 @@ namespace numerical_methods_Newton
 {
     public partial class FormNewtonMethod : Form
     {
-        
-
-        /*private double setPrecision()
-        {
-            
-        }*/
-
         private double f(double x)
         {
             try
             {
                 String str = tb_function.Text.Replace("x", ""+x);
                 str = str.Replace(",", ".");
-                //MessageBox.Show(str);
                 Expression e1 = new Expression(str);
-                //MessageBox.Show("Parser: " + e1.getExpressionString());
                 double result = e1.calculate();
-                //MessageBox.Show("Result: "+ result);
-
-                //String str2 = "-0.333333333333333 * -0.333333333333333 + 3 * -0.333333333333333 + 1";
-                //Expression e2 = new Expression(str2);
-                //double result2 = e2.calculate();
-                //MessageBox.Show("Result 2: " + result2);
-
                 return result;
-                //return Math.Round(result, (int)ud_places_comma.Value);
             }
             catch (ArgumentNullException e)
             {
                 MessageBox.Show("Brak podanej funkcji!");
             }
             return 1;
-            //return Math.Pow(x, 2) + 3 * x + 1;
         }
 
         private double fp(double x)
         {
-            //Expression eder = new Expression("der(x^2 + 3*x + 1), x, 0");
-            //MessageBox.Show("Pochodna x^2 + 3*x + 1, gdzie x to 0: " + eder.calculate());
-            return 2 * x + 3;
-            /*try
-            {
-                //String functionStr = tb_function.Text;
-
-            }*/
+            Argument xArg = new Argument("x = " + x.ToString().Replace(",","."));
+            Expression eder = new Expression("der(" + tb_function.Text + ",x)", xArg);
+            double result = eder.calculate();
+            MessageBox.Show("Pochodna " + eder.getExpressionString() + ", gdzie x to " + x + ": " + result);
+            return result;
         }
 
         public FormNewtonMethod()
@@ -77,7 +56,7 @@ namespace numerical_methods_Newton
             f0 = f(x0);
             i = (int)ud_iters.Value;
             int copyi = i;
-
+            
             f1 = fp(x0);
             while (i != 0 && (Math.Abs(f1) > epsx) && (Math.Abs(f0) > eps0))
             {
@@ -89,24 +68,24 @@ namespace numerical_methods_Newton
                     break;
                 }
                 x1 = x0;
-                x0 = x0 - f0 / f1; // powinno wyjsc -1/3
+                x0 = x0 - f0 / f1;
                 f0 = f(x0);
                 if ((--i) == 0)
                     MessageBox.Show("Przekroczony limit obiegów!");
             }
             if (i != 0)
             {
-                tb_res_zero.Text = "" + x0;
-                tb_res_stop.Text = "" + x1;
+                tb_res_zero.Text = "" + Math.Round(x0, (int)ud_places_comma.Value);
+                tb_res_stop.Text = "" + Math.Round(x1, (int)ud_places_comma.Value);
                 int res_iter = copyi - i;
                 tb_res_num_iter.Text = "" + res_iter;
-                tb_res_zero_function_val.Text = "" + f0;
+                tb_res_zero_function_val.Text = "" + Math.Round(f0, (int)ud_places_comma.Value);
             }
         }
 
         private void b_close_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
     }
 }
